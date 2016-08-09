9deb4eeb328de261c2b8498bd37693a4cee4ee09
Erico Mattos <ericomattos@gmail.com>
2016-08-09 15:12:26 -0300

    New feature to load a configuration file passed by argument

--- projects/jphonelite/src/PhoneApp.java
+++ projects/jphonelite/src/PhoneApp.java
@@ -1,25 +1,38 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 import javaforce.*;
 
 /** Entry point for jphonelite application. */
 
 public class PhoneApp extends JFrame implements WindowListener, WindowController {
   public static void main(String args[]) {
+    final String configFileUrl;
+    if (args.length > 0) {
+        configFileUrl = args[0];
+    } else {
+        configFileUrl = null;
+    }
     java.awt.EventQueue.invokeLater(new Runnable() {
       public void run() {
-        new PhoneApp().setVisible(true);
+        new PhoneApp(configFileUrl).setVisible(true);
       }
     });
   }
 
   private PhonePanel panel;
 
-  private PhoneApp() {
+  private PhoneApp(String configFileUrl) {
     if (!BasePhone.lockFile()) {
       JF.showError("Error", "Another instance of jPhoneLite is already running!");
       System.exit(0);
+    }
+    if (configFileUrl != null) {
+        setupConfig(configFileUrl);
     }
     panel = new PhonePanel(this, false);
     addWindowListener(this);
@@ -32,6 +45,24 @@ public class PhoneApp extends JFrame implements WindowListener, WindowController
     setIconImage(icon.getImage());
     setPosition();
   }
+  
+  public void setupConfig(String url) {
+    System.out.println("Loading config : " + url);
+    try {
+      BufferedReader reader = new BufferedReader(new InputStreamReader(new URL(url).openStream()));
+      FileOutputStream fos = new FileOutputStream(JF.getUserPath() + "/.jphone.xml");
+      String line;
+      do {
+        line = reader.readLine();
+        if (line == null) break;
+        fos.write(line.getBytes());
+      } while (true);
+      fos.close();
+    } catch (Exception e) {
+      System.out.println("Error loading config : " + e);
+    }
+  }
+  
 //interface WindowListener
   public void windowOpened(WindowEvent e) { }
   public void windowClosing(WindowEvent e) {
